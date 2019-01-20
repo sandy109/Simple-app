5433cc39a2beadc2cfbe504960a9bfafb1a09c07
Gaurav <gauravpurohit409@gmail.com>
2019-01-20 16:05:15 +0530

    change in urls.py for about page

--- pages/tests.py
+++ pages/tests.py
@@ -8,5 +8,5 @@ class SimpleTests(SimpleTestCase):
         self.assertEqual(response.status_code,200)
 
     def test_about_page_status_code(self):
-        response = self.client.get('/about')
+        response = self.client.get('/about/')
         self.assertEqual(response.status_code,200)
