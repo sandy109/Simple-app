94428cbfba7d06cb8bc25f2b54159d5b3ae0e9ba
sandy109 <57231720+sandy109@users.noreply.github.com>
2019-10-31 23:02:58 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -5,9 +5,9 @@ import sys
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pages_project.settings')
     try:
-        from django.core.management import execute_from_command_line
+        from django.core.management import 
     except ImportError as exc:
-        raise ImportError(
+        raise Importerror(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
